d2334e34358edf2bc9c612b5e56c4600cca0daa2
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-07-26 17:57:11 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,24 +9,38 @@ function addTodo(){
     var liText = document.createTextNode(todo_task.value)
     li.appendChild(liText)
 
-    
+    //create delete button
     var delBtn = document.createElement("button")
     var delText = document.createTextNode("Remove")
     delBtn.setAttribute("class","btn")  
-    delBtn.setAttribute("onclick","deleteTask()")  
+    delBtn.setAttribute("onclick","deleteTask(this)")  //when this is passed as argument, it calls entire button when function is called
+    //if 'this' is writeen in string 'this' it will return word this otherwise if will return element
     delBtn.appendChild(delText)
 
+    //create edit button
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("Edit")
+    editBtn.setAttribute("class","editbtn")
+    editBtn.appendChild(editText)
+
+
     li.appendChild(delBtn)
-    
+    li.appendChild(editBtn)
+
     list.appendChild(li) 
     
     todo_task.value =""
 
-    console.log(li)
+    // console.log(li)
 } 
 
-function deleteTask(){
-    console.log("checker")
+function deleteTask(e){             //e is a parameter any character/word canbe used elsewise
+    // console.log(e.parentNode)
+    e.parentNode.remove()
+}
+
+function deleteAll(){
+    list.innerHTML=""
 }
 
 // addTodo()
