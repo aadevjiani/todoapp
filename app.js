a67a1d668cfc503897b13835955446f7eae1bf90
aadevjiani <57323312+aadevjiani@users.noreply.github.com>
2020-07-26 19:06:53 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function addTodo(){
     var liText = document.createTextNode(todo_task.value)
     li.appendChild(liText)
 
-    
+    //create delete button
     var delBtn = document.createElement("button")
     var delText = document.createTextNode("Remove")
     delBtn.setAttribute("class","btn")  
@@ -17,8 +17,17 @@ function addTodo(){
     //if 'this' is writeen in string 'this' it will return word this otherwise if will return element
     delBtn.appendChild(delText)
 
+    //create edit button
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("Edit/Update")
+    editBtn.setAttribute("class","editbtn")
+    editBtn.setAttribute("onclick","editTask(this)")
+    editBtn.appendChild(editText)
+
+
     li.appendChild(delBtn)
-    
+    li.appendChild(editBtn)
+
     list.appendChild(li) 
     
     todo_task.value =""
@@ -29,6 +38,14 @@ function addTodo(){
 function deleteTask(e){             //e is a parameter any character/word canbe used elsewise
     // console.log(e.parentNode)
     e.parentNode.remove()
+}
+
+function editTask(e){
+    // e.parentNode.firstChild.nodeValue = "ali"
+    var val = e.parentNode.firstChild.nodeValue
+    var editValue = prompt("Enter value to Update",val)
+    // console.log(e.parentNode.firstChild)
+    e.parentNode.firstChild.nodeValue = editValue
 }
 
 function deleteAll(){
